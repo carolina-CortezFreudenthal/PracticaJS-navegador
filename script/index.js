cc585819a9392aa3dd92c695aa9635d8329f8009
carolina-CortezFreudenthal <carolicortfreu@gmail.com>
2022-12-10 01:38:57 +0100

    terminado

--- script/index.js
+++ script/index.js
@@ -1,25 +1,36 @@
 let transactions = [];
 
+function fillSummary() {
+    let totalIncome = 0;
+    let totalExpense = 0;
+
+    transactions.forEach((transaction) => {
+        if (transaction.type == "expense") totalExpense += transaction.amount;
+        if (transaction.type == "income") totalIncome += transaction.amount;
+    });
+    //ahorro
+    const savings = totalIncome - totalExpense;
+    //Agrego al html
+    document.getElementsByName("totalincome")[0].innerHTML = `$${totalIncome}`;
+    document.getElementsByName("totalexpense")[0].innerHTML = `$${totalExpense}`;
+    document.getElementsByName("savings")[0].innerHTML = `$${savings}`;
+}
+
 function fillTable() {
     const tableBody = document.getElementById("tablebody");
     tableBody.innerHTML = "";
 
     newHtml = "";
-    for (let i = 0; i< transactions.length; i++) {
+    for (let i = transactions.length - 1; i >= 0; i--) {
         const t = transactions[i];
         newHtml += `<tr>` + 
         `<td>${t.concept}</td>` + 
         `<td>${t.amount}</td>` + 
         `<td>${t.type}</td>` + 
-        `<td><button class="deleteButton" onclick="deleteTransaction(${i})">Borrar</button></td>` +
+        `<td><button class="deletebutton" onclick="deleteTransaction(${i})">Borrar</button></td>` +
         `</tr>\n`
     }
     tableBody.innerHTML = newHtml;
-}
-
-function deleteTransaction(id) {
-    transactions = transactions.filter((_t, i) => i !== id);
-    fillTable();
 }
 
 function validateForm (concept, amount) {
@@ -34,27 +45,61 @@ function validateForm (concept, amount) {
     return true;
 }
 
-function addTransaction() {
-    const concept = document.getElementById("inputconcept").value;
+function addTransaction(e) {
+    e.preventDefault();
+
+    const concept = e.target.elements[0].value;
     const transactionType = document.querySelector('input[name="transactiontype"]:checked').value;
-    const amount = document.getElementById("inputamount").value;
+    const amount = e.target.elements[3].value;
 
     const isValid = validateForm(concept, amount);
     
-    // If not valid, then let's break execution of the function
+    // Si no es valido, luego detiene la funcion
     if (!isValid) return;
 
-    // Push into our state in which we store the transaction
+    // Ingresamos los datos de la trasaccion
     transactions.push({
         concept: concept,
         type: transactionType,
         amount: parseFloat(amount), 
     })
 
-    // Clean the form
-    document.getElementById("inputconcept").value = "";
-    document.getElementById("inputamount").value = "";
-
-    // Append the transactions into the table
+    // Agrega la transaction a la tabla
     fillTable();
+    // actualiza el estado 
+    fillSummary()
+    // Va a local storage
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+    // Reset form
+    e.target.reset();
 }
+
+function deleteTransaction(id) {
+    transactions = transactions.filter((t, i) => i !== id);
+
+    // Agrego a la tabla
+    fillTable();
+    // actualizo el estado de las transacciones
+    fillSummary()
+    // va a  local storage
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+
+// Cuando se recargue
+window.onload = () => {
+    // carga las trasactions del local storage
+    transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+    // llenar tabla inicial 
+    fillTable();
+
+    // llenar  
+    fillSummary();
+
+    // a;ado el form una vez que se pulse btn
+    const form = document.getElementById('transactionform');
+    form.addEventListener(('submit'), addTransaction);
+};
+
+/////////
